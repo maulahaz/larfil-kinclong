9b082306f1efbedd9731047c724700660898901d
maulahaz <maulahaz@gmail.com>
2024-12-15 15:22:35 +0400

    Awal-awal

---
+++ app/Models/Owner.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+// use App\Models\Patient;
+
+class Owner extends Model
+{
+    use HasFactory;
+
+    // public function patients()
+    // {
+    //     return $this->hasMany(Patient::class);
+    // }
+
+    public function patients(): HasMany
+    {
+        return $this->hasMany(Patient::class);
+    }
+}
